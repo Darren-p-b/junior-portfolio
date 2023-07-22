e5b95483cb33ee97162be3e2515c2709e439c26d
Darren Broomhall <d.p.broomhall@gmail.com>
2023-07-22 13:25:52 +0100

    navbar built, styling still needs adding

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 // Time to build a portfolio!
 // 0. Are there any requirements? Packages? Libraries? 
@@ -35,19 +36,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Navbar />
       </header>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,16 @@
 // Imports -> variables -> functions -> return statement -> export
 
 // Imports
-
+import './styles.css';
+import React from 'react';
+import LinkedIn from './Linked-In-logo.png';
+import Github from './Github-logo.png';
 // Variables
     // link variables for Home, About, Projects, Contact to find the element by id
-//     const home = document.getElementById('home');
-//     const about = document.getElementById('about');
-//     const projects = document.getElementById('projects');
-//     const contact = document.getElementById('contact');
+    const home = document.getElementById('home');
+    const about = document.getElementById('about');
+    const projects = document.getElementById('projects');
+    const contact = document.getElementById('contact');
     
 //     // link variables for personal links online 
 //     const github = ""
@@ -17,16 +20,20 @@
 // // Functions
 
 // // Return statement
-// // return (
-// //     <div>
-// //         <ul>
-// //             <li><a href='#home'>Home</a></li>
-// //             <li><a href='#about'>About</a></li>
-// //             <li><a href='#projects'>Projects</a></li>
-// //             <li><a href='#contact'>Contact</a></li>
-// //         </ul>
-// //     </div>
-// // )
+const Navbar = () => {
+return (
+    <div id='navbar'>
+        <img src={LinkedIn} alt="My logo" className='navbar-logo'/>
+        <ul className="navbar-ul">
+            <li><a href='#home'>Home</a></li>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#projects'>Projects</a></li>
+            <li><a href='#contact'>Contact</a></li>
+        </ul>
+        <a href="https://www.linkedin.com/in/d-p-broomhall-ffx4shb19ffx4shb19/"><img src={LinkedIn} alt="Linked-in logo" className='navbar-socials' /></a>
+        <a href="https://github.com/Darren-p-b"><img src={Github} alt="Github logo" className='navbar-socials'/></a>
+    </div>
+)}
 // // {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
 // //     <a className="navbar-brand" href="#">Navbar</a>
 // //     <button className="navbar-toggler" type="button" data-toggle="collapse"
@@ -37,5 +44,5 @@
 // //     <a href={github} target="_blank" rel="noopener noreferrer">Github</a>
 // // </nav> */}
 // // Export
-// export default Navbar
+export default Navbar
 // // Styling with bootstrap?
